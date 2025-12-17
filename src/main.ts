5eba0867b1b52e331cbaf4baf200a10fadbcda64
Caio Costa <atsocs.cc@gmail.com>
2025-12-17 17:58:03 +0100

    draw dice correct

--- src/main.ts
+++ src/main.ts
@@ -106,6 +106,61 @@ const sketch = (s: p5): void => {
     ],
   };
 
+  const drawDice = (): void => {
+    // draw a unit dice with pips
+    // draws a box of size 1 centered at origin
+    s.push();
+    s.normalMaterial();
+    s.noStroke();
+    s.box(1);
+    s.pop();
+    const pipRadius = 0.06;
+    const faceOffset = 0.5;
+    const pipOffset = 0.2;
+
+    // Draw pips for each face
+    const faceValues = [1, 2, 3, 4, 5, 6];
+    const faceNormals = [
+      [0, 0, 1], // front
+      [1, 0, 0], // right
+      [0, 0, -1], // back
+      [-1, 0, 0], // left
+      [0, 1, 0], // top
+      [0, -1, 0], // bottom
+    ];
+
+    for (let i = 0; i < faceValues.length; i += 1) {
+      const faceValue = faceValues[i];
+      const normal = faceNormals[i];
+      const pips = dicePipPattern[faceValue];
+
+      s.push();
+      s.translate(
+        (normal[0] * faceOffset),
+        (normal[1] * faceOffset),
+        (normal[2] * faceOffset)
+      );
+      if (normal[0] !== 0) {
+        s.rotateY(normal[0] * 90);
+      } else if (normal[1] !== 0) {
+        s.rotateX(-normal[1] * 90);
+      } else if (normal[2] === -1) {
+        s.rotateY(180);
+      }
+
+      for (const pip of pips) {
+        s.push();
+        s.translate(pip[0] * pipOffset, pip[1] * pipOffset, 0.01);
+        s.fill(10);
+        s.noStroke();
+        s.sphere(pipRadius);
+        s.pop();
+      }
+
+      s.pop();
+    }
+  }
+
   const drawDiceForCell = (
     cellIndex: number,
     isHovered: boolean
@@ -119,6 +174,15 @@ const sketch = (s: p5): void => {
     s.specularMaterial(255);
     s.stroke(210);
     s.strokeWeight(1);
+
+    s.push();
+    const rotationSpeed = 0.6;
+    s.rotateX(s.frameCount * rotationSpeed + cellIndex * 15);
+    s.rotateY(s.frameCount * rotationSpeed * 0.7 + cellIndex * 25);
+    s.rotateZ(s.frameCount * rotationSpeed * 0.4 + cellIndex * 10);
+    s.scale(diceSize);
+    drawDice();
+    s.pop();
 
     s.push();
     s.noStroke();
